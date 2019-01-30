78131dfa34053ffefa627bb545d92bf0778c22b3
Pavel Pristupa <pristupa@gmail.com>
2019-01-31 01:58:37 +0700

    Add ArgumentsResolver

--- tests/test_argument_resolver.py
+++ tests/test_argument_resolver.py
@@ -1,8 +1,9 @@
+import itertools
+
 import pytest
 from mock import Mock
 
-from winter import GenericArgumentResolver
-from winter.argument_resolver import resolve_arguments, register_argument_resolver
+from winter import GenericArgumentResolver, ArgumentsResolver
 from winter.controller import ControllerMethodArgument, ControllerMethod
 
 
@@ -21,7 +22,7 @@ def test_generic_argument_resolver_is_supported(arg_name, arg_type, resolver_arg
     is_supported = generic_argument_resolver.is_supported(argument)
 
     # Assert
-    assert is_supported == expected_supported
+    assert is_supported is expected_supported
     resolve_argument_mock.assert_not_called()
 
 
@@ -43,62 +44,65 @@ def test_generic_argument_resolver_resolve_argument():
     assert result == expected_result
 
 
-def test_resolve_arguments_returns_empty_dict_for_empty_arguments(clear_resolvers):
+def test_resolve_arguments_returns_empty_dict_for_empty_arguments():
     def func():
         pass
 
     expected_resolved_arguments = {}
     controller_method = ControllerMethod(func, '', '')
+    arguments_resolver = ArgumentsResolver()
 
     # Act
-    resolved_arguments = resolve_arguments(controller_method, http_request=Mock(), path_variables={})
+    resolved_arguments = arguments_resolver.resolve_arguments(controller_method, http_request=Mock(), path_variables={})
 
     # Assert
     assert resolved_arguments == expected_resolved_arguments
 
 
-def test_resolve_arguments_resolves_argument_with_the_first_resolver(clear_resolvers):
+def test_resolve_arguments_resolves_argument_with_the_first_resolver():
     def func(a: int):
         pass
 
     expected_resolved_value = 1
     expected_resolved_arguments = {'a': expected_resolved_value}
     controller_method = ControllerMethod(func, '', '')
+    arguments_resolver = ArgumentsResolver()
     resolver = Mock()
     resolver.is_supported.return_value = True
     resolver.resolve_argument.return_value = expected_resolved_value
-    register_argument_resolver(resolver)
+    arguments_resolver.add_argument_resolver(resolver)
 
     # Act
-    resolved_arguments = resolve_arguments(controller_method, http_request=Mock(), path_variables={})
+    resolved_arguments = arguments_resolver.resolve_arguments(controller_method, http_request=Mock(), path_variables={})
 
     # Assert
     assert resolved_arguments == expected_resolved_arguments
 
 
-def test_resolve_arguments_resolves_argument_with_the_second_resolver(clear_resolvers):
+def test_resolve_arguments_resolves_argument_with_the_second_resolver():
     def func(a: int):
         pass
 
     expected_resolved_value = 1
     expected_resolved_arguments = {'a': expected_resolved_value}
     controller_method = ControllerMethod(func, '', '')
+    arguments_resolver = ArgumentsResolver()
     resolver1 = Mock()
     resolver1.is_supported.return_value = False
-    register_argument_resolver(resolver1)
+    arguments_resolver.add_argument_resolver(resolver1)
     resolver2 = Mock()
     resolver2.is_supported.return_value = True
     resolver2.resolve_argument.return_value = expected_resolved_value
-    register_argument_resolver(resolver2)
+    arguments_resolver.add_argument_resolver(resolver2)
 
     # Act
-    resolved_arguments = resolve_arguments(controller_method, http_request=Mock(), path_variables={})
+    resolved_arguments = arguments_resolver.resolve_arguments(controller_method, http_request=Mock(), path_variables={})
 
     # Assert
     assert resolved_arguments == expected_resolved_arguments
 
 
-def test_resolve_arguments_resolves_argument_with_the_path_variable(clear_resolvers):
+def test_resolve_arguments_resolves_argument_with_the_path_variable():
     def func(a: int):
         pass
 
@@ -107,24 +111,48 @@ def test_resolve_arguments_resolves_argument_with_the_path_variable(clear_resolv
     expected_resolved_arguments = {arg_name: expected_resolved_value}
     controller_method = ControllerMethod(func, '', '')
     path_variables = {arg_name: str(expected_resolved_value)}
+    arguments_resolver = ArgumentsResolver()
 
     # Act
-    resolved_arguments = resolve_arguments(controller_method, http_request=Mock(), path_variables=path_variables)
+    resolved_arguments = arguments_resolver.resolve_arguments(
+        controller_method,
+        http_request=Mock(),
+        path_variables=path_variables,
+    )
 
     # Assert
     assert resolved_arguments == expected_resolved_arguments
 
 
-def test_resolve_arguments_fails(clear_resolvers):
+def test_resolve_arguments_fails():
     def func(a: int):
         pass
 
     arg_name = 'a'
     controller_method = ControllerMethod(func, '', '')
-    global _resolvers
-    _resolvers = []
+    arguments_resolver = ArgumentsResolver()
 
     # Assert
     with pytest.raises(Exception, match=f'Unable to resolve argument {arg_name}: int'):
         # Act
-        resolve_arguments(controller_method, http_request=Mock(), path_variables={})
+        arguments_resolver.resolve_arguments(controller_method, http_request=Mock(), path_variables={})
+
+
+@pytest.mark.parametrize('resolver1_supported, resolver2_supported', itertools.product([False, True], repeat=2))
+def test_arguments_resolver_is_supported_true(resolver1_supported, resolver2_supported):
+    arguments_resolver = ArgumentsResolver()
+    resolver1 = Mock(spec_set=GenericArgumentResolver)
+    resolver1.is_supported.return_value = resolver1_supported
+    arguments_resolver.add_argument_resolver(resolver1)
+    resolver2 = Mock(spec_set=GenericArgumentResolver)
+    resolver2.is_supported.return_value = resolver2_supported
+    arguments_resolver.add_argument_resolver(resolver2)
+    argument = object()
+
+    # Act
+    is_supported = arguments_resolver.is_supported(argument)
+
+    # Assert
+    assert is_supported is (resolver1_supported or resolver2_supported)
+    resolver1.is_supported.assert_called_once_with(argument)
+    assert resolver2.is_supported.called is not resolver1_supported
--- winter/argument_resolver.py
+++ winter/argument_resolver.py
@@ -1,8 +1,10 @@
 import typing
 from abc import ABC
 from abc import abstractmethod
+from typing import Any
 from typing import Callable
 from typing import Dict
+from typing import List
 from typing import Type
 
 from rest_framework.request import Request
@@ -10,7 +12,9 @@ from rest_framework.request import Request
 from .controller import ControllerMethod
 from .controller import ControllerMethodArgument
 
-_resolvers = []
+
+class NotSupported(Exception):
+    pass
 
 
 class ArgumentResolver(ABC):
@@ -22,6 +26,48 @@ class ArgumentResolver(ABC):
     @abstractmethod
     def resolve_argument(self, argument: ControllerMethodArgument, http_request: Request):
         pass
+
+
+class ArgumentsResolver(ArgumentResolver):
+    def __init__(self):
+        super().__init__()
+        self._argument_resolvers: List[ArgumentResolver] = []
+
+    def add_argument_resolver(self, argument_resolver: ArgumentResolver):
+        self._argument_resolvers.append(argument_resolver)
+
+    def is_supported(self, argument: ControllerMethodArgument) -> bool:
+        return any(argument_resolver.is_supported(argument) for argument_resolver in self._argument_resolvers)
+
+    def resolve_argument(self, argument: ControllerMethodArgument, http_request: Request) -> Any:
+        try:
+            argument_resolver = next(
+                argument_resolver for argument_resolver in self._argument_resolvers
+                if argument_resolver.is_supported(argument)
+            )
+            return argument_resolver.resolve_argument(argument, http_request)
+        except StopIteration:
+            raise NotSupported()
+
+    def resolve_arguments(
+            self,
+            controller_method: ControllerMethod,
+            http_request: Request,
+            path_variables: Dict,
+    ) -> Dict[str, Any]:
+        type_hints = typing.get_type_hints(controller_method.func)
+        type_hints.pop('return', None)
+        resolved_arguments = {}
+        for argument in controller_method.arguments:
+            try:
+                resolved_arguments[argument.name] = self.resolve_argument(argument, http_request)
+            except NotSupported:
+                if argument.name not in path_variables:
+                    raise Exception(f'Unable to resolve argument {argument.name}: {argument.type_.__name__}')
+                str_value = path_variables[argument.name]
+                resolved_arguments[argument.name] = argument.type_(str_value)
+
+        return resolved_arguments
 
 
 class GenericArgumentResolver(ArgumentResolver):
@@ -37,33 +83,4 @@ class GenericArgumentResolver(ArgumentResolver):
         return self._resolve_argument(http_request)
 
 
-def register_argument_resolver(resolver: ArgumentResolver):
-    _resolvers.append(resolver)
-
-
-def resolve_arguments(
-        controller_method: ControllerMethod,
-        http_request: Request,
-        path_variables: Dict,
-) -> Dict[str, object]:
-    type_hints = typing.get_type_hints(controller_method.func)
-    type_hints.pop('return', None)
-    resolved_arguments = {}
-    for argument in controller_method.arguments:
-        resolver = find_resolver(argument)
-        if resolver:
-            resolved_arguments[argument.name] = resolver.resolve_argument(argument, http_request)
-            continue
-        if argument.name in path_variables:
-            str_value = path_variables[argument.name]
-            resolved_arguments[argument.name] = argument.type_(str_value)
-            continue
-        raise Exception(f'Unable to resolve argument {argument.name}: {argument.type_.__name__}')
-    return resolved_arguments
-
-
-def find_resolver(argument: ControllerMethodArgument) -> typing.Optional[ArgumentResolver]:
-    for resolver in _resolvers:
-        if resolver.is_supported(argument):
-            return resolver
-    return None
+arguments_resolver = ArgumentsResolver()
