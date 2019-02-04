3397a8b8db2581f7596ab626d705a5a9c3d035fc
Andrey Berenda <berenda.andrey@gmail.com>
2019-02-04 11:55:25 +0700

    delete schema.setup()

--- winter/schema/__init__.py
+++ winter/schema/__init__.py
@@ -1,5 +1,3 @@
-from enum import Enum
-
 from .controller_method_inspector import ControllerMethodInspector
 from .controller_method_inspector import get_controller_method_inspectors
 from .controller_method_inspector import register_controller_method_inspector
@@ -7,8 +5,3 @@ from .enum_inspector import inspect_enum_class
 from .generation import generate_swagger_for_operation
 from .path_parameters_inspector import PathParametersInspector
 from .query_parameters_inspector import QueryParametersInspector
-
-
-def setup():
-    from drf_yasg.inspectors.field import hinting_type_info
-    hinting_type_info.insert(0, (Enum, inspect_enum_class))
