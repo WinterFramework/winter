827eabcb0b57d32d620db68d68dba8b3ddcbd2f8
Andrey Berenda <berenda.andrey@gmail.com>
2019-02-06 00:25:50 +0700

    delete not need settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,6 @@ def pytest_configure():
     from django.conf import settings
 
     settings.configure(
-        INSTALLED_APPS=[
-            'django.contrib.contenttypes',
-            'django.contrib.auth',
-            'rest_framework',
-            'tests',
-        ],
         ROOT_URLCONF='tests.urls'
     )
     django.setup()
