1456c8acf99deeab8ef6c8a10d4863ffcc1a8a10
Andrey Berenda <andrey.berenda@quantumsoft.ru>
2019-02-06 11:33:34 +0700

    Add tests for pagination

---
+++ tests/pagination/test_page_position_argument_inspector.py
@@ -0,0 +1,28 @@
+import pytest
+from mock import Mock
+
+from winter.controller import ControllerMethod
+from winter.controller import ControllerMethodArgument
+from winter.pagination import PagePosition
+from winter.pagination import PagePositionArgumentInspector
+from winter.pagination import PagePositionArgumentResolver
+
+
+
+@pytest.mark.parametrize(('argument_type', 'expected_count_parameters'), (
+        (PagePosition, 2),
+        (object, 0),
+))
+def test_page_position_argument_inspector(argument_type, expected_count_parameters):
+    resolver = PagePositionArgumentResolver()
+    inspector = PagePositionArgumentInspector(resolver)
+    controller_method = Mock(spec=ControllerMethod)
+    argument = Mock(spec=ControllerMethodArgument)
+    argument.type_ = argument_type
+    controller_method.arguments = [argument]
+
+    # Act
+    parameters = inspector.inspect_parameters(controller_method)
+
+    # Assert
+    assert len(parameters) == expected_count_parameters
