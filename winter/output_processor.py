f2dda43897b48a03a4d903281e54c1d5c0f1a4d4
Andrey Berenda <berenda.andrey@gmail.com>
2019-02-07 09:31:55 +0700

    Change importgs and namespace

--- winter/output_processor.py
+++ winter/output_processor.py
@@ -1,18 +1,15 @@
 import abc
-from abc import abstractmethod
-from typing import Any
-from typing import List
-from typing import Optional
+import typing
 
-from rest_framework.request import Request
+from rest_framework.request import Request as DRFRequest
 
 
 class IOutputProcessor(abc.ABC):
     """Process controller method returned value so that it can be put to HttpResponse body.
     Common usage is to serializer some DTO to dict."""
 
-    @abstractmethod
-    def process_output(self, output, request: Request):
+    @abc.abstractmethod
+    def process_output(self, output, request: DRFRequest):
         return output
 
 
@@ -22,20 +19,20 @@ class IOutputProcessorResolver(abc.ABC):
     Due to python dynamic typing it's called after every controller method call.
     """
 
-    @abstractmethod
-    def is_supported(self, body: Any) -> bool:
+    @abc.abstractmethod
+    def is_supported(self, body: typing.Any) -> bool:
         return False
 
-    @abstractmethod
-    def get_processor(self, body: Any) -> IOutputProcessor:
+    @abc.abstractmethod
+    def get_processor(self, body: typing.Any) -> IOutputProcessor:
         pass
 
 
 _registered_output_processors = {}
-_registered_resolvers: List[IOutputProcessorResolver] = []
+_registered_resolvers: typing.List[IOutputProcessorResolver] = []
 
 
-def register_output_processor(func, output_processor: IOutputProcessor):
+def register_output_processor(func: typing.Callable, output_processor: IOutputProcessor):
     if func in _registered_output_processors:
         raise Exception(f'{func} already has registered output processor')
     _registered_output_processors[func] = output_processor
@@ -45,7 +42,7 @@ def register_output_processor_resolver(output_processor_resolver: IOutputProcess
     _registered_resolvers.append(output_processor_resolver)
 
 
-def get_output_processor(func, body: Any) -> Optional[IOutputProcessor]:
+def get_output_processor(func, body: typing.Any) -> typing.Optional[IOutputProcessor]:
     output_processor = _registered_output_processors.get(func)
     if output_processor is not None:
         return output_processor
