31e958053dcb079189758854d360d8af71c0bd91
Alexander Egorov <mofr@zond.org>
2019-02-13 20:00:23 +0700

    Fix tests

--- tests/schema/test_response_schema.py
+++ tests/schema/test_response_schema.py
@@ -1,33 +1,34 @@
 from typing import Dict
+from typing import Type
 
 import pytest
 from drf_yasg import openapi
 
-from winter.controller import ControllerMethod
 from winter.controller import get_controller_component
 from winter.schema.generation import build_responses_schemas
 from ..controllers import ControllerWithExceptions
 
-controller_component = get_controller_component(ControllerWithExceptions)
 
-
-@pytest.mark.parametrize('controller_method, expected_responses', [
-    (controller_component.get_method('declared_and_thrown'), {
+@pytest.mark.parametrize('controller_class, method_name, expected_responses', [
+    (ControllerWithExceptions, 'declared_and_thrown', {
         '200': openapi.Schema(type=openapi.TYPE_STRING),
         '400': openapi.Schema(type=openapi.TYPE_OBJECT, properties={
             'message': openapi.Schema(type=openapi.TYPE_STRING),
         }),
     }),
-    (controller_component.get_method('not_declared_but_thrown'), {
+    (ControllerWithExceptions, 'not_declared_but_thrown', {
         '200': openapi.Schema(type=openapi.TYPE_STRING),
     }),
-    (controller_component.get_method('declared_but_no_handler'), {
+    (ControllerWithExceptions, 'declared_but_no_handler', {
         '200': openapi.Schema(type=openapi.TYPE_STRING),
     }),
 ])
-def test_response_schema(controller_method: ControllerMethod, expected_responses: Dict):
+def test_response_schema(controller_class: Type, method_name: str, expected_responses: Dict):
+    controller_component = get_controller_component(controller_class)
+    method = controller_component.get_method(method_name)
+
     # Act
-    responses = build_responses_schemas(controller_method)
+    responses = build_responses_schemas(method)
 
     # Assert
     assert responses == expected_responses
--- winter/routing/routing.py
+++ winter/routing/routing.py
@@ -1,6 +1,6 @@
 from typing import Dict
-from typing import List
 from typing import Optional
+from typing import Tuple
 
 from dataclasses import dataclass
 
@@ -13,37 +13,37 @@ _routes: Dict[object, 'Route'] = {}
 class Route:
     url_path: str
     http_method: str = None
-    produces: List[MediaType] = None  # It's used for swagger only at the moment, but will be used in routing later
+    produces: Tuple[MediaType] = None  # It's used for swagger only at the moment, but will be used in routing later
 
 
-def route(url_path: str, http_method: Optional[str] = None, produces: Optional[List[MediaType]] = None):
+def route(url_path: str, http_method: Optional[str] = None, produces: Optional[Tuple[MediaType]] = None):
     def wrapper(func):
         register_route(func, url_path, http_method, produces)
         return func
     return wrapper
 
 
-def route_get(url_path='', produces: Optional[List[MediaType]] = None):
+def route_get(url_path='', produces: Optional[Tuple[MediaType]] = None):
     return route(url_path, 'GET', produces=produces)
 
 
-def route_post(url_path='', produces: Optional[List[MediaType]] = None):
+def route_post(url_path='', produces: Optional[Tuple[MediaType]] = None):
     return route(url_path, 'POST', produces=produces)
 
 
-def route_delete(url_path='', produces: Optional[List[MediaType]] = None):
+def route_delete(url_path='', produces: Optional[Tuple[MediaType]] = None):
     return route(url_path, 'DELETE', produces=produces)
 
 
-def route_patch(url_path='', produces: Optional[List[MediaType]] = None):
+def route_patch(url_path='', produces: Optional[Tuple[MediaType]] = None):
     return route(url_path, 'PATCH', produces=produces)
 
 
-def route_put(url_path='', produces: Optional[List[MediaType]] = None):
+def route_put(url_path='', produces: Optional[Tuple[MediaType]] = None):
     return route(url_path, 'PUT', produces=produces)
 
 
-def register_route(func, url_path: str, http_method: Optional[str], produces: Optional[List[MediaType]]):
+def register_route(func, url_path: str, http_method: Optional[str], produces: Optional[Tuple[MediaType]]):
     assert func not in _routes, f'{func} is already mapped to a route'
     _routes[func] = Route(url_path, http_method, produces)
 
