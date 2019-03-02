8635cf28f2f8dc32a5ce52665e7fe70944d67d07
Andrey Berenda <berenda.andrey@gmail.com>
2019-03-02 14:04:43 +0700

    Fix tests

--- tests/test_core/test_utils.py
+++ tests/test_core/test_utils.py
@@ -6,7 +6,9 @@ def test_cached_property():
 
         @cached_property
         def method(self):
+            """Docstring"""
             return 1
 
     assert isinstance(_Test.method, cached_property)
     assert _Test().method == 1
+    assert _Test.method.__doc__ == 'Docstring'
