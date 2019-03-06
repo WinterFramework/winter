5ea0bcd2e18f780fea3a01c5d77c83d9062af683
Andrey Berenda <berenda.andrey@gmail.com>
2019-03-06 19:31:48 +0700

    Add trailing comma

--- winter/core/annotation_decorator.py
+++ winter/core/annotation_decorator.py
@@ -47,7 +47,7 @@ class annotate_class(annotate_base):
 
 
 class annotate_method(annotate_base):
-    _supported_classes = (ComponentMethod, types.FunctionType)
+    _supported_classes = (ComponentMethod, types.FunctionType,)
 
     def __call__(self, func_or_method: typing.Union[types.FunctionType, ComponentMethod]):
         if isinstance(func_or_method, ComponentMethod):
