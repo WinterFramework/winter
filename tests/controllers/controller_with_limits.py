b3cbd296459281a0e309d3ce76bfd41ecccd3414
Pavel V. Pristupa <pristupa@gmail.com>
2019-04-18 19:54:14 +0700

    Add tests

--- tests/controllers/controller_with_limits.py
+++ tests/controllers/controller_with_limits.py
@@ -1,4 +1,5 @@
 import winter
+from winter.pagination import MaximumLimitValueExceeded
 from winter.pagination import PagePosition
 from winter.pagination import RedirectToDefaultLimitException
 
@@ -6,10 +7,11 @@ from winter.pagination import RedirectToDefaultLimitException
 @winter.route('with-limits/')
 class ControllerWithLimits:
 
-    @winter.route_get('with-redirect-to-default/')
+    @winter.route_get('')
     @winter.pagination.limits(default=20, maximum=100, redirect_to_default=True)
     @winter.throws(RedirectToDefaultLimitException)  # TODO: add auto throws here
-    def with_redirect_to_default(self, page_position: PagePosition):
+    @winter.throws(MaximumLimitValueExceeded)  # TODO: add auto throws here
+    def method(self, page_position: PagePosition):
         return {
             'limit': page_position.limit,
             'offset': page_position.offset,
--- tests/test_controller_with_limits.py
+++ tests/test_controller_with_limits.py
@@ -5,23 +5,34 @@ from rest_framework.test import APIClient
 from tests.entities import AuthorizedUser
 
 
-def test_controller_with_limits_with_redirect_to_default_redirects():
+def test_controller_with_limits_redirects_to_default_limit():
     client = APIClient()
     user = AuthorizedUser()
     client.force_authenticate(user)
 
-    response = client.get('/with-limits/with-redirect-to-default/')
+    response = client.get('/with-limits/')
 
     assert response.status_code == HTTPStatus.FOUND
-    assert response['Location'] == '/with-limits/with-redirect-to-default/?limit=20'
+    assert response['Location'] == '/with-limits/?limit=20'
 
 
-def test_controller_with_limits_with_redirect_to_default_ok():
+def test_controller_with_limits_does_not_redirect_if_limit_is_set():
     client = APIClient()
     user = AuthorizedUser()
     client.force_authenticate(user)
 
-    response = client.get('/with-limits/with-redirect-to-default/?limit=30')
+    response = client.get('/with-limits/?limit=30')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'limit': 30, 'offset': None}
+
+
+def test_controller_with_limits_fails_if_maximum_limit_is_exceeded():
+    client = APIClient()
+    user = AuthorizedUser()
+    client.force_authenticate(user)
+
+    response = client.get('/with-limits/?limit=120')
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == 'Maximum limit value is exceeded: 100'
