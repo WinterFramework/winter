812b50916d13181e48167f6bc26903bec1447882
Andrey Berenda <berenda.andrey@gmail.com>
2019-06-23 18:57:34 +0700

    Fixed inspector and added tests

--- winter/pagination/page_position_argument_inspector.py
+++ winter/pagination/page_position_argument_inspector.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 
 from drf_yasg import openapi
 
+from winter.pagination.sort import get_allowed_order_by_fields
 from .page_position import PagePosition
 from .page_position_argument_resolver import PagePositionArgumentResolver
 from ..schema import MethodArgumentsInspector
@@ -13,6 +14,7 @@ if TYPE_CHECKING:
 
 class PagePositionArgumentsInspector(MethodArgumentsInspector):
     def __init__(self, page_position_argument_resolver: PagePositionArgumentResolver):
+        self._page_position_argument_resolver = page_position_argument_resolver
         self.limit_parameter = openapi.Parameter(
             name=page_position_argument_resolver.limit_name,
             description='Number of results to return per page',
@@ -27,11 +29,32 @@ class PagePositionArgumentsInspector(MethodArgumentsInspector):
             in_=openapi.IN_QUERY,
             type=openapi.TYPE_INTEGER,
         )
+        self.default_order_by_parameter = openapi.Parameter(
+            name=page_position_argument_resolver.order_by_name,
+            description='Comma separated order by fields',
+            required=False,
+            in_=openapi.IN_QUERY,
+            type=openapi.TYPE_INTEGER,
+        )
 
     def inspect_parameters(self, route: 'Route') -> List[openapi.Parameter]:
         parameters = []
+        allowed_order_by_fields = get_allowed_order_by_fields(route.method)
+        if not allowed_order_by_fields:
+            order_by_parameter = self.default_order_by_parameter
+        else:
+            order_by_parameter = openapi.Parameter(
+                name=self._page_position_argument_resolver.order_by_name,
+                description='Comma separated order by fields',
+                required=False,
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_INTEGER,
+                enum=list(allowed_order_by_fields),
+            )
+
         for argument in route.method.arguments:
             if argument.type_ == PagePosition:
                 parameters.append(self.limit_parameter)
                 parameters.append(self.offset_parameter)
+                parameters.append(order_by_parameter)
         return parameters
