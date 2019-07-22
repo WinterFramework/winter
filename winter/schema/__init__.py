b8d6ac4274aee074c33c4686e780c3f685438b8a
Andrey Berenda <berenda.andrey@gmail.com>
2019-07-22 16:54:12 +0700

    Added disabling generating swagger (#127)
    * Added disabling generating swagger
    
    * Changed version
    
    * Small refactoring
    
    * Changed SwaggerAutoSchema
    
    * Added tests
    
    * Renamed some methods and set them property
    
    * Fixed import
    
    * Deleted not need variable

--- winter/schema/__init__.py
+++ winter/schema/__init__.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 from .enum_inspector import inspect_enum_class
-from .generation import generate_swagger_for_operation
 from .method_arguments_inspector import MethodArgumentsInspector
 from .method_arguments_inspector import get_method_arguments_inspectors
 from .method_arguments_inspector import register_controller_method_inspector
