03f8b416bef715c1ba923e01799f81928c6dff21
Andrey Berenda <berenda.andrey@gmail.com>
2019-07-30 15:04:37 +0700

    Fixed cached property: raise AttributeError on setting attribute (#130)

--- tests/core/test_utils.py
+++ tests/core/test_utils.py
@@ -1,14 +1,34 @@
+import pytest
+
 from winter.core.utils import cached_property
 
 
-def test_cached_property():
-    class _Test:
+class _Entity:
 
-        @cached_property
-        def method(self):
-            """Docstring"""
-            return 1
+    def __init__(self):
+        self._counter = 0
 
-    assert isinstance(_Test.method, cached_property)
-    assert _Test().method == 1
-    assert _Test.method.__doc__ == 'Docstring'
+    @cached_property
+    def method(self):
+        """Docstring"""
+        self._counter += 1
+        return self._counter
+
+
+def test_cached_property_getter():
+    assert isinstance(_Entity.method, cached_property)
+    entity = _Entity()
+
+    for _ in range(5):
+        assert entity.method == 1
+
+    assert _Entity.method.__doc__ == 'Docstring'
+
+
+def test_cached_property_setter():
+    entity = _Entity()
+
+    with pytest.raises(AttributeError) as exception:
+        entity.method = 3
+
+    assert exception.value.args == ("can't set attribute",)
--- winter/core/utils.py
+++ winter/core/utils.py
@@ -9,5 +9,10 @@ class cached_property:
     def __get__(self, instance, cls=None):
         if instance is None:
             return self
+        if self.name in instance.__dict__:
+            return instance.__dict__[self.name]
         res = instance.__dict__[self.name] = self.func(instance)
         return res
+
+    def __set__(self, instance, value):
+        raise AttributeError("can't set attribute")
