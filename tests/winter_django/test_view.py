4fc6d23e60849400f148618e4a90d1d549144dcf
Maxim Luzin <Lumax.rus@gmail.com>
2020-10-06 15:48:59 +0300

    Wrap long strint to parentheses

--- tests/winter_django/test_view.py
+++ tests/winter_django/test_view.py
@@ -3,9 +3,11 @@ from winter_django import create_django_urls
 
 
 def test_create_django_urls():
-    expected_url_pattern = '^controller_with_path_parameters/(?P<param1>[^/]+)/(?P<param2>\\d+)/'\
-        '(?P<param3>((one)|(two)))/(?P<param4>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/'\
+    expected_url_pattern = (
+        '^controller_with_path_parameters/(?P<param1>[^/]+)/(?P<param2>\\d+)/'
+        '(?P<param3>((one)|(two)))/(?P<param4>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/'
         '(?P<param5>((1)|(2)))/$'
+    )
 
     urls = create_django_urls(ControllerWithPathParameters)
 
