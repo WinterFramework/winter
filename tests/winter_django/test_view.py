3a15e6d5ec4d5abd8e966a32a18f2611efaadbe5
Maxim Luzin <Lumax.rus@gmail.com>
2020-10-06 14:12:56 +0300

    Fix create_django_urls for build correct regex pattern

---
+++ tests/winter_django/test_view.py
@@ -0,0 +1,13 @@
+from tests.controllers.controller_with_path_parameters import ControllerWithPathParameters
+from winter_django import create_django_urls
+
+
+def test_create_django_urls():
+    expected_url_pattern = '^controller_with_path_parameters/(?P<param1>[^/]+)/(?P<param2>\\d+)/'\
+        '(?P<param3>((one)|(two)))/(?P<param4>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/'\
+        '(?P<param5>((1)|(2)))/$'
+
+    urls = create_django_urls(ControllerWithPathParameters)
+
+    assert len(urls) == 1
+    assert urls[0].pattern.regex.pattern == expected_url_pattern
