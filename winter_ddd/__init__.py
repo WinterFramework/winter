293a6972e236b26587c696ebf0584a6801926f65
Pavel V. Pristupa <pristupa@gmail.com>
2020-11-10 14:24:16 +0700

    Handle domain events upon CRUDRepository.save()
    Fixes feature-177

--- winter_ddd/__init__.py
+++ winter_ddd/__init__.py
@@ -3,5 +3,3 @@ from .domain_event import DomainEvent
 from .domain_event_handler import domain_event_handler
 from .domain_event_handler import global_domain_event_dispatcher
 from .domain_events import DomainEvents
-from .entity import Entity
-from .value_object import ValueObject
