edbdbd407ffd4e3a82673e37cdb71355e9be7ba8
burize <varimartas@yandex.ru>
2020-11-12 21:13:11 +0300

    Add test case for domain handlers' order execution

--- tests/winter_ddd/test_process_domain_events.py
+++ tests/winter_ddd/test_process_domain_events.py
@@ -3,7 +3,6 @@ from typing import Union
 
 import pytest
 
-from winter_ddd import AggregateRoot
 from winter_ddd import DomainEvent
 from winter_ddd import domain_event_handler
 from winter_ddd import global_domain_event_dispatcher
@@ -58,8 +57,48 @@ def empty_handler():
     yield Handler()
 
 
-class Aggregate(AggregateRoot):
+class DomainEventForOrder(DomainEvent):
     pass
+
+
+class OtherDomainEventForOrder(DomainEvent):
+    pass
+
+
+class OrderHandler1:
+    handled_domain_events = []
+
+    @domain_event_handler
+    def handle_one(self, domain_event: DomainEventForOrder):
+        self.handled_domain_events.append('handle_one_1')
+
+    @domain_event_handler
+    def handle_list_one(self, domain_events: List[DomainEventForOrder]):
+        self.handled_domain_events.append('handle_list_one_1')
+
+    @domain_event_handler
+    def handle_many(self, domain_events: List[Union[DomainEventForOrder, OtherDomainEventForOrder]]):
+        self.handled_domain_events.append('handle_many_1')
+
+
+class OrderHandler2:
+    handled_domain_events = []
+
+    @domain_event_handler
+    def handle_many(self, domain_events: List[Union[DomainEventForOrder, OtherDomainEventForOrder]]):
+        self.handled_domain_events.append('handle_many_2')
+
+
+class OrderHandler3:
+    handled_domain_events = []
+
+    @domain_event_handler
+    def handle_many(self, domain_events: Union[DomainEventForOrder, OtherDomainEventForOrder]):
+        self.handled_domain_events.append('handle_many_3')
+
+    @domain_event_handler
+    def handle_one(self, domain_event: DomainEventForOrder):
+        self.handled_domain_events.append('handle_one_3')
 
 
 def test_process_domain_events(empty_handler):
@@ -83,3 +122,32 @@ def test_process_domain_events(empty_handler):
     assert empty_handler.handled_many_another_domain_events == [[another_domain_event]]
     assert empty_handler.handled_union_domain_events == [domain_event1, another_domain_event, domain_event2]
     assert empty_handler.handled_union_list_domain_events == [[domain_event1, another_domain_event, domain_event2]]
+
+
+def test_order_process_domain_events():
+    domain_event = DomainEventForOrder()
+
+    handled_events = []
+    OrderHandler1.handled_domain_events = handled_events
+    OrderHandler2.handled_domain_events = handled_events
+    OrderHandler3.handled_domain_events = handled_events
+
+    # Act
+    for _ in range(5):
+        global_domain_event_dispatcher.dispatch([
+            domain_event,
+        ])
+
+    # Assert
+    while handled_events:
+        events = handled_events[:6]
+        del handled_events[:6]
+        assert events == [
+            'handle_one_1',
+            'handle_list_one_1',
+            'handle_one_3',
+            'handle_many_1',
+            'handle_many_2',
+            'handle_many_3',
+        ]
+
