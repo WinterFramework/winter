09ef1037e9d9ccca53aabf0ed23ad16993bca7c4
mikhail.lazko <mikhail.lazko@quantumsoft.ru>
2020-12-08 11:26:37 +0700

    Add type hints

--- winter_openapi/annotations/global_exception.py
+++ winter_openapi/annotations/global_exception.py
@@ -10,11 +10,11 @@ class GlobalExceptionAnnotation:
     exception_cls: Type[Exception]
 
 
-def global_exception(exception_class):
+def global_exception(exception_class: Type[Exception]) -> Type[Exception]:
     return register_global_exception(exception_class)
 
 
-def register_global_exception(exception_class):
+def register_global_exception(exception_class: Type[Exception]) -> Type[Exception]:
     assert issubclass(exception_class, Exception), f'Class "{exception_class}" must be a subclass of Exception'
     annotation = GlobalExceptionAnnotation(exception_cls=exception_class)
     annotation_decorator = annotate(annotation, unique=True)
