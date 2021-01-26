34b718f1d05963003ca1ec93cc398a3bb40b187f
Pavel V. Pristupa <pavel.pristupa@quantumsoft.ru>
2021-01-26 18:00:18 +0700

    Add optional method argument in Interceptor.pre_handle

--- tests/interceptors.py
+++ tests/interceptors.py
@@ -1,6 +1,7 @@
 from rest_framework.request import Request
 
 import winter
+from winter.core import ComponentMethod
 from winter.web import Configurer
 from winter.web import Interceptor
 from winter.web import InterceptorRegistry
@@ -9,7 +10,15 @@ from winter.web import ResponseHeader
 
 class HelloWorldInterceptor(Interceptor):
     @winter.response_header('x-hello-world', 'hello_world_header')
-    def pre_handle(self, request: Request, hello_world_header: ResponseHeader[str]):
+    @winter.response_header('x-method', 'method_header')
+    def pre_handle(
+        self,
+        method: ComponentMethod,
+        request: Request,
+        method_header: ResponseHeader[str],
+        hello_world_header: ResponseHeader[str],
+    ):
+        method_header.set(method.full_name)
         if 'hello_world' in request.query_params:
             hello_world_header.set('Hello, World!')
 
