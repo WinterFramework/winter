f1c8650f51ad06e7243cfaa085b920321477e540
Pavel V. Pristupa <pavel.pristupa@quantumsoft.ru>
2021-06-22 11:29:48 +0700

    Add winter.core.Injector

--- winter/core/__init__.py
+++ winter/core/__init__.py
@@ -9,6 +9,7 @@ from .component_method import ComponentMethod
 from .component_method import component_method
 from .component_method_argument import ArgumentDoesNotHaveDefault
 from .component_method_argument import ComponentMethodArgument
+from .injection import Injector
 from .injection import get_injector
 from .injection import set_injector
 from .module_discovery import ModuleDiscovery
--- winter/core/injection.py
+++ winter/core/injection.py
@@ -1,15 +1,41 @@
 from typing import Optional
+from typing import Type
+from typing import TypeVar
 
-from injector import Injector
+import injector
 
-_injector: Optional[Injector] = None
+_Interface = TypeVar('_Interface')
 
 
-def set_injector(injector: Injector) -> None:
+class Injector:
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        self._injector: injector.Injector = None
+
+    def create_object(self, cls: Type[_Interface], additional_kwargs=None) -> _Interface:
+        return self._get_injector().create_object(cls, additional_kwargs)
+
+    def get(self, interface: Type[_Interface], scope=None) -> _Interface:
+        return self._get_injector().get(interface, scope)
+
+    def _get_injector(self) -> injector.Injector:
+        if not self._injector:
+            self._injector = injector.Injector(*self._args, **self._kwargs)
+        return self._injector
+
+    def __getattr__(self, item):
+        return getattr(self._get_injector(), item)
+
+
+def set_injector(injector_: Injector) -> None:
     global _injector
-    _injector = injector
+    _injector = injector_
 
 
 def get_injector() -> Injector:
     global _injector
     return _injector
+
+
+_injector: Optional[Injector] = None
