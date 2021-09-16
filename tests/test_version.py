de4ef2276b3d71deecc6575e27b496a15213c27e
Nikolay Zhukov <nikolay.zhukov@quantumsoft.ru>
2021-09-16 11:16:13 +0700

    build with poetry, bump version to 9.4.1

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,10 +1,10 @@
-from winter.__version__ import __version__
+from pkg_resources import get_distribution
 from semver import parse
 
 
 def test_version_is_a_valid_semver():
     # Act
-    version = parse(__version__)
+    version = parse(get_distribution('winter').version)
 
     # Assert
     assert version['major'] > 0  # In fact we test that parse doesn't fail with ValueError
