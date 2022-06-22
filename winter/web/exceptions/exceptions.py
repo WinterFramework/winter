b9b37a9fdff16bee0eb8a1570054bc677c5aed9a
Pavel Pristupa <pavel@Pavels-MacBook-Pro.local>
2022-06-22 22:50:24 +0200

    Fix UUID validation

--- winter/web/exceptions/exceptions.py
+++ winter/web/exceptions/exceptions.py
@@ -6,3 +6,9 @@ from .problem import problem
 @problem(status=HTTPStatus.TOO_MANY_REQUESTS, detail='Request was throttled')
 class ThrottleException(Exception):
     pass
+
+
+class RedirectException(Exception):
+    def __init__(self, redirect_to: str):
+        super().__init__()
+        self.redirect_to = redirect_to
--- winter/web/exceptions/redirect_exception.py
+++
@@ -1,4 +0,0 @@
-class RedirectException(Exception):
-    def __init__(self, redirect_to: str):
-        super().__init__()
-        self.redirect_to = redirect_to
