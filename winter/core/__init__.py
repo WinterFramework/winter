1fa4a235c8d2b2b2d4c4eeb093d1643093cec746
Sergey Bondarenko <sergey.bondarenko@quantumsoft.ru>
2022-09-01 12:05:28 +0200

    Fix version for Poetry testing, clean __init__.py

--- winter/core/__init__.py
+++ winter/core/__init__.py
@@ -11,7 +11,4 @@ from .component_method_argument import ArgumentDoesNotHaveDefault
 from .component_method_argument import ComponentMethodArgument
 from .injection import get_injector
 from .injection import set_injector
-from .module_discovery import get_all_classes
-from .module_discovery import get_all_subclasses
-from .module_discovery import import_recursively
 from .utils import cached_property
