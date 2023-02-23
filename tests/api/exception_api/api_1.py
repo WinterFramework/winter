e923ab384486869083fabf66bb970d4da3724d8c
Evgeny Smekalin <evgeny.smekalin@gmail.com>
2023-02-23 11:46:06 +0100

    Move api package with exception to inline declaration in test

--- tests/api/exception_api/api_1.py
+++
@@ -1,7 +0,0 @@
-import winter
-
-
-class API1:
-    @winter.route_get('exception/')
-    def get(self):  # pragma: no cover
-        pass
--- tests/api/exception_api/api_2.py
+++
@@ -1,8 +0,0 @@
-import winter
-
-
-@winter.web.no_authentication
-class API2:
-    @winter.route_patch('exception/')
-    def update(self):  # pragma: no cover
-        pass
