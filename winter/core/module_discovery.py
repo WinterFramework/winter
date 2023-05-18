2c4eaf1099dded3f7587832b2d1a77731adcff68
Pavel V. Pristupa <pavel.pristupa@quantumsoft.pro>
2023-05-18 14:46:57 +0200

    Fix

--- winter/core/module_discovery.py
+++ winter/core/module_discovery.py
@@ -8,7 +8,7 @@ from typing import Tuple
 from typing import Type
 
 
-def get_all_classes(package: Union[str, ModuleType]) -> Generator[Tuple[str, Type], None, None]:
+def get_all_classes(package: str) -> Generator[Tuple[str, Type], None, None]:
     classes_set = set()
     for module in import_recursively(package):
         try:
@@ -25,24 +25,24 @@ def get_all_subclasses(supertype: Type) -> Generator[Tuple[str, Type], None, Non
     classes_set = set()
     for module in dict(sys.modules).values():
         try:
-            for class_name, class_ in inspect.getmembers(module, inspect.isclass):
-                if class_ in classes_set:
-                    continue
-                try:
-                    # Workaround to not fail in the issubclass check
-                    from _weakref import ReferenceType
-
-                    ReferenceType(class_)
-                except TypeError:
-                    continue
-                if class_ is not supertype and issubclass(class_, supertype):
-                    yield class_name, class_
+            members = inspect.getmembers(module, inspect.isclass)
         except ImportError:
             pass
+        for class_name, class_ in members:
+            if class_ in classes_set:
+                continue
+            try:
+                # Workaround to not fail in the issubclass check
+                from _weakref import ReferenceType
+
+                ReferenceType(class_)
+            except TypeError:
+                continue
+            if class_ is not supertype and issubclass(class_, supertype):
+                yield class_name, class_
 
 
-def import_recursively(package: Union[str, ModuleType]) -> Generator[ModuleType, None, None]:
-    if isinstance(package, str):
-        package = importlib.import_module(package)
+def import_recursively(package: str) -> Generator[ModuleType, None, None]:
+    package = importlib.import_module(package)
     for loader, name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
         yield importlib.import_module(name)
