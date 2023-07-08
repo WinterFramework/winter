69b5c57873c26988b02fdd92cb13ea0e66d83db5
Alexander Egorov <mofr@zond.org>
2023-07-08 14:38:58 +0200

    Add test_validate_spec

--- tests/winter_openapi/test_validators.py
+++ tests/winter_openapi/test_validators.py
@@ -2,7 +2,10 @@ from http import HTTPStatus
 
 import pytest
 
+import winter
 from winter.web import problem
+from winter.web.routing import get_route
+from winter_openapi import generate_openapi
 from winter_openapi import validate_missing_raises_annotations
 
 
@@ -14,3 +17,21 @@ def test_validate_missing_raises_annotations_with_missed_raises_and_not_global_e
 
     with pytest.raises(AssertionError, match=expected_error_message):
         validate_missing_raises_annotations()
+
+
+@pytest.mark.parametrize('validate', [True, False])
+def test_validate_spec(validate):
+    """
+    There is no way to check the case when the spec is invalid.
+    So it's mostly to execute both branches of the code and check that it doesn't fail.
+    """
+    class _TestAPI:
+        @winter.route_get('/test-api/')
+        def endpoint(self):  # pragma: no cover
+            pass
+
+    route = get_route(_TestAPI.endpoint)
+
+    # Act
+    generate_openapi(title='title', version='1.0.0', routes=[route], validate=validate)
+
