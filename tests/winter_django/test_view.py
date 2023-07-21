ae5eade38264a8e72fbd9d05cb5acd1853e42597
Alexander Egorov <mofr@zond.org>
2023-07-21 22:21:51 +0200

    SUSHI-13086 Pass json instead of urlencoded form in test_create_django_urls_from_routes

--- tests/winter_django/test_view.py
+++ tests/winter_django/test_view.py
@@ -6,8 +6,8 @@ def test_create_django_urls_from_routes(api_client):
     url = f"/notes/?note_id={uuid4()}"
 
     get_http_response = api_client.get(url)
-    post_http_response = api_client.post("/notes/", data=dict(name="Name"))
-    patch_http_response = api_client.patch(url, data=dict(name="New Name"))
+    post_http_response = api_client.post("/notes/", json=dict(name="Name"))
+    patch_http_response = api_client.patch(url, json=dict(name="New Name"))
 
     assert get_http_response.status_code == HTTPStatus.OK
     assert post_http_response.status_code == HTTPStatus.OK
