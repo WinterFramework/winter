6a7f8da9ca7c1aadf6d38ca9c772d7a9a6644266
Evgeny Smekalin <evgeny.smekalin@gmail.com>
2024-04-22 21:00:29 +0200

    Add emit_many to EventPublisher. Update CHANGELOG.md

--- winter/messaging/event_publisher.py
+++ winter/messaging/event_publisher.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from abc import abstractmethod
+from typing import List
 
 from .event import Event
 
@@ -7,4 +8,8 @@ from .event import Event
 class EventPublisher(ABC):
     @abstractmethod
     def emit(self, event: Event):
+        pass
+
+    @abstractmethod
+    def emit_many(self, events: List[Event]):
         pass
