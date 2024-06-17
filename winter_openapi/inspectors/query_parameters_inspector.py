acc0150b591b9bb703db7038523fab2b9fdd1c80
Pavel V. Pristupa <pavel.pristupa@quantumsoft.pro>
2024-06-17 20:15:39 +0200

    FIx coverage

--- winter_openapi/inspectors/query_parameters_inspector.py
+++ winter_openapi/inspectors/query_parameters_inspector.py
@@ -25,6 +25,7 @@ class QueryParametersInspector(RouteParametersInspector):
         if annotation is not None:
             argument = next(
                 (argument for argument in route.method.arguments if argument.name == annotation.argument_name),
+                None,
             )
             query_parameters = route.get_query_parameters()
             query_parameters_map = {query_parameter.name: query_parameter for query_parameter in query_parameters}
